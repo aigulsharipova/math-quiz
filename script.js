de14be65afa4de0670bf0c99510ee5bf41fb6ab9
Aigul <sharipova.aig@gmail.com>
2022-08-06 12:55:17 -0400

    function

--- script.js
+++ script.js
@@ -2,5 +2,27 @@ const button = document.querySelector('.btn');
 button.addEventListener('click', result);
 
 function result(e) {
-  e.preventDefault
+  e.preventDefault();
+
+  let points = 0;
+
+  if(document.querySelector('#correctAnswerOne').checked) {
+    points ++;
+  }
+
+  if(document.querySelector('#correctAnswerTwo').checked) {
+    points ++;
+  }
+
+  if(document.querySelector('#correctAnswerThree').checked) {
+    points ++;
+  }
+
+  if(document.querySelector('#correctAnswerFour').checked) {
+    points ++;
+  }
+  
+  if(document.querySelector('#correctAnswerFive').checked) {
+    points ++;
+  }
 }
